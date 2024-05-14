6412aaec24e22bf9f96989b10958e390e07e1a3c
promehteo <117484469+promehteo@users.noreply.github.com>
2024-05-14 11:41:05 -0400

    menu 2.0

--- Menu/menu.py
+++ Menu/menu.py
@@ -1,17 +1,35 @@
 import opciones.administrador as administrador
 import opciones.index as index
 
+index.borrar_pantalla()
+
 print("Bienvenido a MENU CLASSROOM, por favor selecione una opcion")
 print("1.- alumno")
 print("2.- profesor")
-seleccion_usuario = administrador.input_modificado()
 
-if seleccion_usuario == "1":
-    index.ejecutar_mein_alumno()
+def menu():
+        
+    while True:
 
-elif seleccion_usuario == "2":
-    administrador.ejecutar_mein_administrador()
+        seleccion_usuario = administrador.input_modificado()
 
+        if seleccion_usuario == "1":
+            index.ejecutar_mein_alumno()
+            break
 
+        elif seleccion_usuario == "2":
+            administrador.ejecutar_mein_administrador()
+            break
+
+        else:
+            print("Por favor selecione una opcion valida")
+
+def main():
+    menu()
+
+if __name__ == "__main__":
+    main()
+
+print("Gracias por usar nuestro codigo")
 
 
